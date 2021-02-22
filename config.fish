f4659a0a8617b908502470ac0fc56c048edafc00
Lukus <lukusaurelius@gmail.com>
2021-02-21 19:19:57 -0600

    Fixed a major bug with fish_ssh_agent; did a complete rewrite

--- config.fish
+++ config.fish
@@ -2,4 +2,13 @@
 
 starship init fish | source
 fish_ssh_agent
+
+set -l key_location ~/.ssh/lukusaurelius
+if __ssh_agent_is_started
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $status -eq 0 ]
+        ssh-add $key_location
+    end
+end
+
 #ssh-add $HOME/.ssh/lukusaurelius
