c7ab4cdabfd7fa53c7418125a252ad1133273341
Lukus <lukusaurelius@gmail.com>
2021-02-21 19:26:31 -0600

    made the key adder check that the key exists before trying to add it

--- config.fish
+++ config.fish
@@ -7,6 +7,7 @@ set -l key_location ~/.ssh/lukusaurelius
 if __ssh_agent_is_started
     ssh-add -l | grep "The agent has no identities" > /dev/null
     if [ $status -eq 0 ]
+        and test -e $key_location
         ssh-add $key_location
     end
 end
